cb2853bce836e062a98adb69a897280940f5b0c0
Ian Chege <en20101832020@student.mut.ac.ke>
2024-03-30 18:17:13 +0300

    feat: orgId authorization

--- convex/files.ts
+++ convex/files.ts
@@ -1,5 +1,19 @@
 import { ConvexError, v } from "convex/values"
-import { mutation, query } from "./_generated/server"
+import { MutationCtx, QueryCtx, mutation, query } from "./_generated/server"
+import { getUser } from "./users"
+
+async function hasAccessToOrg(
+  ctx: QueryCtx | MutationCtx,
+  tokenIdentifier: string,
+  orgId: string
+) {
+  const user = await getUser(ctx, tokenIdentifier)
+
+  const hasAccess =
+    user.orgIds.includes(orgId) || user.tokenIdentifier.includes(orgId)
+
+  return hasAccess
+}
 
 export const createFile = mutation({
   args: {
@@ -10,6 +24,18 @@ export const createFile = mutation({
     const identity = await ctx.auth.getUserIdentity()
     if (!identity) {
       throw new ConvexError("User must be authenticated to upload a file")
+    }
+
+    const hasAccess = await hasAccessToOrg(
+      ctx,
+      identity.tokenIdentifier,
+      args.orgId
+    )
+
+    if (!hasAccess) {
+      throw new ConvexError(
+        "You do not have permission to upload a file to this organization"
+      )
     }
 
     await ctx.db.insert("files", {
@@ -28,7 +54,15 @@ export const getFiles = query({
     if (!identity) {
       return []
     }
+    const hasAccess = await hasAccessToOrg(
+      ctx,
+      identity.tokenIdentifier,
+      args.orgId
+    )
 
+    if (!hasAccess) {
+      return []
+    }
     return ctx.db
       .query("files")
       .withIndex("by_orgId", (q) => q.eq("orgId", args.orgId))
