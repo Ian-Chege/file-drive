ffe8f6dad52fe5871590541e26be3c01f5e5d165
Ian Chege <en20101832020@student.mut.ac.ke>
2024-04-06 12:55:39 +0300

    feat: Add Loader2 component to show loading state when fetching files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import { useQuery } from "convex/react"
 import { useOrganization, useUser } from "@clerk/nextjs"
 import Image from "next/image"
+import { Loader2 } from "lucide-react"
 
 import { api } from "../../convex/_generated/api"
 import { UploadButton } from "@/components/dashboard/upload-button"
@@ -16,10 +17,17 @@ export default function Home() {
     orgId = organization.organization?.id ?? user.user?.id
   }
   const files = useQuery(api.files.getFiles, orgId ? { orgId } : "skip")
+  const isLoading = files === undefined
 
   return (
     <main className="container mx-auto p-12">
-      {files && files.length === 0 && (
+      {isLoading && (
+        <div className="flex flex-col gap-8 w-full items-center mt-24">
+          <Loader2 className="h-24 w-24 animate-spin text-gray-500" />
+          <div className="text-2xl">Loading...</div>
+        </div>
+      )}
+      {!isLoading && files.length === 0 && (
         <div className="flex flex-col gap-8 w-full items-center mt-24">
           <Image
             src="/empty-state.svg"
@@ -34,7 +42,7 @@ export default function Home() {
         </div>
       )}
 
-      {files && files.length > 0 && (
+      {!isLoading && files.length > 0 && (
         <>
           <div className="flex justify-between items-center mb-8">
             <h1 className="text-4xl font-bold">Your files</h1>
